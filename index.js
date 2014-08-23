d3f32ecc405eb945190c25f8f2d69257b26330b1
ceclinux <src655@gmail.com>
2014-08-23 23:10:11 +0000

    完成基本功能

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ var request = require('request');
 var PREURL = 'http://www.etymonline.com/index.php?term='
 var AFTERURL = '&allowed_in_frame=0'
 var jsdom = require("jsdom");
+var printcolor = require('./printcolor');
 
-var getwords = function  (word) {
+
+module.exports = function  (word) {
 jsdom.env(
 PREURL + word + AFTERURL,
   ["http://code.jquery.com/jquery.js"],
   function (errors, window) {
-    console.log(window.$("dd").eq(0).text());
+    console.log(printcolor(window.$("dd").eq(0).text()));
   }
 );
-
 }
 
-getwords('lone');
---
+++ printcolor.js
@@ -0,0 +1,88 @@
+require('colorful').colorful()
+//var tomagenta = function (data) {
+//var dataarray = data.split(/"/);
+//var colordata = '';
+//for (var i = 0; i < dataarray.length; i++) {
+//if (i%2==0) {
+//colordata += dataarray[i];
+//}else{
+//colordata += (("\"" + dataarray[i] + "\"").to.magenta.color)
+//}
+//};
+//return colordata;
+//}
+
+//module.exports = function(data) {
+//var bluedata = '';
+//var dataarray = data.split(/\(|\)/);
+//for (var i = 0; i < dataarray.length; i++) {
+//if (i%2==0) {
+//bluedata += tomagenta(dataarray[i]);
+//}else{
+//bluedata += ((tomagenta("(" + dataarray[i] + ")")).to.italic.color)
+//}
+//};
+//return bluedata;
+//}
+var flag = 0;
+module.exports = function  (data) {
+  var d = '';
+  for (var i = 0; i < data.length; i++) {
+    var color = getcolor(getpar(data[i]));
+    if(color == 'italic' ){
+      if (flag) {
+        d += data[i].to.italic.underline.blue.color;
+      }else{
+        d += data[i].to.italic.blue.color;
+      }
+    }else if(color == 'green'){
+      d += data[i].to.underline.green.color;
+    }else if(color == 'magenta'){
+      if (flag) {
+        d += data[i].to.underline.magenta.color;
+      }else{
+        d += data[i].to.magenta.color;
+      }
+    }else{
+      d += data[i];
+    }
+  };
+  return d;
+}
+
+var stack = '';
+
+var isstackempty = function  () {
+  return stack == '';
+}
+
+var getpar = function (c){
+  if((c == ')'&&stack.slice(-1) == '(')||(c == ']' && stack.slice(-1) == '[')||(c == stack.slice(-1) && c == '\"')){
+    stack = stack.slice(0, -1);
+    return c;
+  }else if(c == '[' || c == '(' || c == '\"'){
+    stack += c;
+  }
+  if(!isstackempty()){
+    return stack.slice(-1);
+  }
+  return '';
+}
+
+var getcolor = function  (c) {
+  if(c == '(' || c== ')'){
+    return "italic";
+  }else if(c == '[' || c == ']'){
+    if( c == '['){
+      flag = 1;
+    }else{
+      flag = 0;
+    }
+    return "green";
+  }else if(c == '\"'){
+    return "magenta";
+  }else{
+    return "white";
+  }
+}
+

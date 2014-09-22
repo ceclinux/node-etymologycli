e3425991494083b74970de5e03667310ef1ac73c
ceclinux <src655@gmail.com>
2014-09-22 01:48:20 +0000

    使用jquery.min解析，加快速度

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-node-etymologycli ![](https://d25lcipzij17d.cloudfront.net/badge.png?id=js&type=3d&v=0.0.7)
+node-etymologycli ![](https://d25lcipzij17d.cloudfront.net/badge.png?id=js&type=3d&v=0.0.8)
 ---
 
 **A command line tool of http://www.etymonline.com/**
--- index.js
+++ index.js
@@ -9,7 +9,7 @@ module.exports = function  (word) {
 //这样的URL ww.etymonline.com/index.php?term=word&allowed_in_frame=0，只出现一个单词
 jsdom.env(
 PREURL + word + AFTERURL,
-  ["http://code.jquery.com/jquery.js"],
+  ["http://code.jquery.com/jquery.min.js"],
   function (errors, window) {
     console.log(printcolor(window.$("dd").eq(0).text()));
   }
